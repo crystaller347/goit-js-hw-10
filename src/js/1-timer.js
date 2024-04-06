4d7849a892e084dceb5c913a03cf828590b43e6a
crystaller347 <qwertymum04@gmail.com>
2024-04-06 16:00:54 +0300

    fill onClose function

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,16 +1,48 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const button = document.querySelector("button");
+const input = document.querySelector("#datetime-picker");
+let userSelectedDate;
+let timeInterval;
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    userSelectedDate = selectedDates[0];
+    timeInterval = userSelectedDate - options.defaultDate;
+    if (timeInterval < 0) {
+      window.alert("Please choose a date in the future");
+      button.disabled = true;
+    } else {
+      button.disabled = false;
+    }
   },
 };
 
 flatpickr("#datetime-picker", options);
 
-let userSelectedDate;
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
