20a0d4a64aa7abdf38843ec1bd4d0978d502877d
crystaller347 <qwertymum04@gmail.com>
2024-04-06 17:03:28 +0300

    do js homework 10

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,11 +1,9 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
 
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-const button = document.querySelector("button");
-const input = document.querySelector("#datetime-picker");
 let userSelectedDate;
 let timeInterval;
 
@@ -17,32 +15,54 @@ const options = {
   onClose(selectedDates) {
     userSelectedDate = selectedDates[0];
     timeInterval = userSelectedDate - options.defaultDate;
+
     if (timeInterval < 0) {
-      window.alert("Please choose a date in the future");
-      button.disabled = true;
+      iziToast.error({
+        color: 'red',
+        position: 'topRight',
+        message: `Please choose a date in the future`,
+      });
+
+      inputTime.disabled = false;
     } else {
-      button.disabled = false;
+      startBtn.disabled = false;
+      inputTime.disabled = true;
     }
   },
 };
 
-flatpickr("#datetime-picker", options);
-
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
-
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
   return { days, hours, minutes, seconds };
 }
+
+const calendar = flatpickr('#datetime-picker', options);
+const inputTime = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('[data-start]');
+const timeValueElements = document.querySelectorAll('.value');
+
+startBtn.disabled = true;
+startBtn.addEventListener('click', event => {
+  const repeatTime = setInterval(() => {
+    timeInterval = userSelectedDate - new Date();
+    
+    if (timeInterval < 0) {
+      startBtn.disabled = true;
+      inputTime.disabled = false;
+      clearInterval(repeatTime);
+      return;
+    }
+    const timer = convertMs(timeInterval);
+    timeValueElements[0].textContent = timer.days.toString().padStart(2, '0');
+    timeValueElements[1].textContent = timer.hours.toString().padStart(2, '0');
+    timeValueElements[2].textContent = timer.minutes.toString().padStart(2, '0');
+    timeValueElements[3].textContent = timer.seconds.toString().padStart(2, '0');
+  }, 1000);
+});
